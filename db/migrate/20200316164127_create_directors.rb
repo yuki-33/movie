8e99400a5ff37854bc21453a39976e387e00baad
yuki_pisces <pisces.myth.3@gmail.com>
2020-03-17 09:47:50 -0400

    add director_table_and_model

---
+++ db/migrate/20200316164127_create_directors.rb
@@ -0,0 +1,10 @@
+class CreateDirectors < ActiveRecord::Migration[6.0]
+  def change
+    create_table :directors do |t|
+      t.string "name"
+      t.string "from"
+
+      t.timestamps
+    end
+  end
+end
