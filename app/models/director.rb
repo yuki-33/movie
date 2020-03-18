7871b333d311787c4078aee8239d4f40457cd047
yuki_pisces <pisces.myth.3@gmail.com>
2020-03-18 09:55:20 -0400

    add model before save

--- app/models/director.rb
+++ app/models/director.rb
@@ -1,4 +1,10 @@
 class Director < ApplicationRecord
   validates :name, presence: true
   scope :by_id, ->{ order(id: :asc) }
+
+  before_save :capitalize_name
+  def capitalize_name
+    self.name = self.name.split.map(&:capitalize).join(' ')
+  end
+  
 end
