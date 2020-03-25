cd0e95344c5c38e1cf31c8ebe357ccf4bbca2511
yuki_pisces <pisces.myth.3@gmail.com>
2020-03-25 13:00:09 -0400

    add paginate

--- Gemfile
+++ Gemfile
@@ -36,6 +36,7 @@ gem 'acts_as_list'
 gem "font-awesome-rails"
 gem 'carrierwave'
 gem 'mini_magick'
+gem 'kaminari'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -2,7 +2,7 @@ class WorksController < ApplicationController
   before_action :set_work, only: [:edit, :show, :update, :destroy]
 
   def index
-    @works = Work.all.by_position
+    @works = Work.includes(:director).by_position.page(params[:page]).per(9)
   end
 
   def create
