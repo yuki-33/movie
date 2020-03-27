591e17c70296519851909f4579dea581e6291c95
yuki_pisces <pisces.myth.3@gmail.com>
2020-03-27 12:11:35 -0400

    rename migration_file

---
+++ db/migrate/20200327160346_create_profiles.rb
@@ -0,0 +1,12 @@
+class CreateProfiles < ActiveRecord::Migration[6.0]
+  def change
+    create_table :profiles do |t|
+      t.integer :user_id
+      t.string :first_name
+      t.string :last_name
+      t.text :introduction
+      t.string :image
+      t.timestamps
+    end
+  end
+end
