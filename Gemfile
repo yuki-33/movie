5ba31f6ed53d7a66c77577c945332e6a7f5c1bb1
yuki_pisces <pisces.myth.3@gmail.com>
2020-03-30 12:30:30 -0400

    add cloudinary

--- Gemfile
+++ Gemfile
@@ -38,6 +38,7 @@ gem 'carrierwave'
 gem 'mini_magick'
 gem 'kaminari'
 gem 'devise'
+gem 'cloudinary'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/uploaders/profile_image_uploader.rb
+++ app/uploaders/profile_image_uploader.rb
@@ -4,7 +4,12 @@ class ProfileImageUploader < BaseImageUploader
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  if Rails.env.production?
+    include Cloudinary::CarrierWave
+  else
+    storage :file
+  end
+
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
