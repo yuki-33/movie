46c1092835e02a667db3af0817635fd83946a2f2
yuki_pisces <pisces.myth.3@gmail.com>
2020-11-11 14:20:34 -0500

    clean index and show

--- app/controllers/login/directors_controller.rb
+++ app/controllers/login/directors_controller.rb
@@ -24,13 +24,6 @@ class Login::DirectorsController < Login::ApplicationController
   end
 
   def update
-    @director.photo.purge if params[:director][:remove_photo_id]
-    if params[:director][:remove_image_ids]
-      params[:director][:remove_image_ids].each do |image_id|
-        image = @director.images.find(image_id)
-        image.purge
-      end
-    end
     # @director.images.purge if params[:director][:remove_images_id]
     if !@director.photo.attached? && @director.photo_blob_id
         @director.photo.attach(ActiveStorage::Blob.find(@director.photo_blob_id))
@@ -42,6 +35,13 @@ class Login::DirectorsController < Login::ApplicationController
       redirect_to director_path(@director)
     else
       render "edit"
+    end
+    @director.photo.purge if params[:director][:remove_photo_id]
+    if params[:director][:remove_image_ids]
+      params[:director][:remove_image_ids].each do |image_id|
+        image = @director.images.find(image_id)
+        image.purge
+      end
     end
   end
 
