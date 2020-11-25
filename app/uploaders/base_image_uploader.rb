ac481dca64d4607d4e432dbc0d588b27c9a79f95
yuki_pisces <pisces.myth.3@gmail.com>
2020-11-25 09:10:26 -0500

    hide uploafder

--- app/uploaders/base_image_uploader.rb
+++ app/uploaders/base_image_uploader.rb
@@ -1,16 +1,16 @@
 # encoding: utf-8
 
-class BaseImageUploader < CarrierWave::Uploader::Base
-
-  def optimize
-    manipulate! do |img|
-      img.strip
-      img.combine_options do |c|
-        c.quality "80"
-        c.depth "8"
-        c.interlace "plane"
-      end
-      img
-    end
-  end
-end
+# class BaseImageUploader < CarrierWave::Uploader::Base
+#
+#   def optimize
+#     manipulate! do |img|
+#       img.strip
+#       img.combine_options do |c|
+#         c.quality "80"
+#         c.depth "8"
+#         c.interlace "plane"
+#       end
+#       img
+#     end
+#   end
+# end
